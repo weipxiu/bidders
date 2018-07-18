e408ba2cd083ee8e7323376b6e45d0a6ea2324ae
lijun <lijun@021.com>
2018-07-18 18:29:54 +0800

    新项目添加

---
+++ src/components/Index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <template>
+      <el-radio v-model="radio" label="1">备选项</el-radio>
+      <el-radio v-model="radio" label="2">备选项</el-radio>
+    </template>
+  </div>
+</template>
+
+<script>
+ export default {
+    data () {
+      return {
+        radio: '1'
+      };
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
